e5abf12cd8cc919d40f6388016650eb45847288d
Ben Hickson <bzhickson@gmail.com>
2020-02-10 00:46:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -106,6 +106,27 @@ def apply_coupons(cart, coupons)
   
   coupons.length.times do |i|
     cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    couponed_item_name = "#{coupons[i][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon
+        # increase count
+        cart_item_with_coupon[:count] += coupons[i][:num]
+        cart_item[:count] -= coupons[i][:num]
+      else
+        # add cart item with coupon
+        cart_item_with_coupon = {
+          item: couponed_item_name,
+          price: (coupons[i][:cost] / coupons[i][:num]).round(2),
+          count: coupons[i][:num],
+          clearance: cart_item[:clearance]
+        }
+        cart.push(cart_item_with_coupon)
+        cart_item[:count] -= coupons[i][:num]
+      end
+    end
+    cart
   end
   
   
